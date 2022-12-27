678466dd7eaecf4088c338065a72e31e40e244db
Shreyas R <shrer7925@gmail.com>
2022-12-27 23:46:53 +0530

    Styled Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,8 +4,22 @@ const Login = () => {
     return(
         <div className="homePageWrapper">
             <div className="formWrapper">
-                <img src ="/codecolab.jpg" alt = "code-colab-logo"/>
+                <img className = "CClogo" src ="/codecolab.jpg" alt = "code-colab-logo"/> 
+                <h4 className="mainLabel">LOGIN</h4> 
+                <div className="inputGroup">
+                <input type= "text" className = "inputBox" placeholder = "USERNAME" /> 
+                    <input type= "text" className = "inputBox" placeholder = "ROOM ID" />
+                    
+                    <button className="btn loginbtn">JOIN</button>
+                    <span className="createinfo">
+                        If you don't have an invite then create a &nbsp;
+                        <a href="" className="createNewBtn">new room</a>
+                    </span>
+                </div>
             </div>
+            <footer> 
+                <h4> Built with ❤️ by <a href="https://github.com/Shreyas-R-7925">REY</a></h4>
+            </footer>
         </div>
     )
 }

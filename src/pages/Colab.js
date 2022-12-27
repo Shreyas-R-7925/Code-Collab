cda1d6eaa67cd7f4756c2040585d8dde73c160ba
Shreyas R <shrer7925@gmail.com>
2022-12-27 22:40:55 +0530

    Added CodeColab logo

---
+++ src/pages/Colab.js
@@ -0,0 +1,9 @@
+import React from "react" 
+
+const Colab = () => {
+    return(
+        <div>EDITOR PAGE</div>
+    )
+}
+
+export default Colab;
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react" 
+
+const Login = () => {
+    return(
+        <div className="homePageWrapper">
+            <div className="formWrapper">
+                <img src ="/codecolab.jpg" alt = "code-colab-logo"/>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
